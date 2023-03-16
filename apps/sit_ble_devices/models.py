e1802d769747172708725ead93144819b9c04402
Sven <sven.hoyer@hotmail.com>
2023-03-16 19:03:18 +0100

    save distance measurements in database

--- apps/sit_ble_devices/models.py
+++ apps/sit_ble_devices/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class DistanceMeasurement(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    distance = models.FloatField()
+
+    class Meta:
+        ordering = ["created"]
