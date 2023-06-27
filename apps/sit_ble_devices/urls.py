39e39a4eafd8a7fabb719b81ffdb5bebea951234
Sven <sven.hoyer@hotmail.com>
2023-06-27 17:04:44 +0200

    remove unused import

--- apps/sit_ble_devices/urls.py
+++ apps/sit_ble_devices/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_simplejwt import views as jwt_views
 
 from .views import (
     UwbDeviceViewSet,
